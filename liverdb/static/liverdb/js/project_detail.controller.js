a8b710ae57305d616484e84cb00402fbc3bb4dab
fuxins <fuxinhust@gmail.com>
2021-02-04 00:46:17 +0800

    project detail

---
+++ liverdb/static/liverdb/js/project_detail.controller.js
@@ -0,0 +1,25 @@
+"use strict";
+
+angular.module('liverdb')
+    .controller('ProjectDetailController', ProjectDetailController);
+
+function PrrojectDetailController($scope,$http,$routeParams,$window) {
+    $scope.pmid=$routeParams.pmid
+    var condition={}
+    console.log("ProjectDetailController loaded");
+    condition['PMID']=$scope.pmid
+    $scope.getproject=function(){
+        $http({
+            url:'/api/project',
+            method:'GET',
+            params:condition
+        }).then(function(response){
+            console.log("project_list")
+            $scope.project=response.data.project_list[0]
+            $scope.project_count=response.data.project_count
+            console.log($scope.project_list);
+
+        })
+    }
+    $scope.getproject()
+}
